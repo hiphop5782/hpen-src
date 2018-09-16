9ded31d301086636073bb4603d14a013bce5feef
hwang <hwang@DESKTOP-72R0437>
2018-09-16 17:09:09 +0900

    돋보기 수정중
    현재 1회 확대만 가능함

--- hPen/src/com/hpen/util/key/GlobalkeyManager.java
+++ hPen/src/com/hpen/util/key/GlobalkeyManager.java
@@ -2,6 +2,7 @@ package com.hpen.util.key;
 
 import java.io.IOException;
 
+import com.hakademy.utility.magnify.MagnificationManager;
 import com.hpen.draw.ui.CaptureFrame;
 import com.hpen.draw.ui.DrawingFrame;
 import com.hpen.livezoom.ui.ZoomFrame;
@@ -9,6 +10,16 @@ import com.tulskiy.keymaster.common.Provider;
 
 public class GlobalkeyManager {
 	static Process magnify_proc;
+//	static class MagnifyThread extends Thread{
+//		public MagnifyThread() {
+//			setDaemon(true);
+//		}
+//		public void run() {
+//			MagnificationManager.getManager().start();
+//		}
+//	}
+//	static Thread magnify_thread;
+	static boolean magnify = false;
 	static {
 		Provider provider = Provider.getCurrentProvider(false);
 		provider.register(KeyManager.alt1, e->{
@@ -21,6 +32,14 @@ public class GlobalkeyManager {
 			ZoomFrame.start();
 		});
 		provider.register(KeyManager.alt4, e->{
+			if(!magnify) {
+				MagnificationManager.getManager().start();
+			}
+			else {
+				MagnificationManager.getManager().stop();
+			}
+		});
+		provider.register(KeyManager.alt5, e->{
 			if(magnify_proc == null) {
 				String command = System.getenv("windir")+"\\system32\\magnify.exe";
 				ProcessBuilder builder = new ProcessBuilder(new String[] {"cmd.exe", "/C", command});
