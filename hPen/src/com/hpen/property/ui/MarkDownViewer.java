821de23068e315f3fa53f6513aba9559ae106702
user <user@DESKTOP-7EQF3M8>
2018-10-10 21:21:32 +0900

    사용설명서 수정
    Markdown이 변환되어 적용되도록 commonmark 적용

--- hPen/src/com/hpen/property/ui/MarkDownViewer.java
+++ hPen/src/com/hpen/property/ui/MarkDownViewer.java
@@ -1,31 +1,43 @@
 package com.hpen.property.ui;
 
+import java.awt.Color;
 import java.awt.Font;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-import javax.swing.JEditorPane;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 import com.hpen.property.ProgramIcon;
 
 public class MarkDownViewer extends JFrame{
-	private JEditorPane editor = new JEditorPane();
-	private JScrollPane scroll = new JScrollPane(editor);
+	private JLabel label = new JLabel();
+	private JScrollPane scroll = new JScrollPane(label);
 	public MarkDownViewer(String text) throws FileNotFoundException {
+		Parser parser = Parser.builder().build();
+		Node document = parser.parse(text);
+		HtmlRenderer renderer = HtmlRenderer.builder().build();
+		text = "<html>"+renderer.render(document)+"</html>";
+		
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocationByPlatform(true);
 		setIconImage(ProgramIcon.getIcon());
 		setTitle(" hPen 사용설명서");
-		setSize(900, 600);
 		add(scroll);
-		editor.setFont(new Font("굴림", Font.PLAIN, 25));
-		editor.setText(text);
-		editor.setEditable(false);
-		this.setVisible(true);
-		editor.setCaretPosition(0);
+		
+		label.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		label.setOpaque(true);
+		label.setBackground(Color.white);
+		label.setText(text);
+		scroll.getVerticalScrollBar().setUnitIncrement(20);
+		setVisible(true);
+		pack();
+		setSize(getWidth() + 30, 500);
 	}
 	
 }
