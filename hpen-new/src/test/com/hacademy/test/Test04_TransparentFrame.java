95844a64e1565765466e9a0ca0bcb95f817eb765
user2 <user2@DESKTOP-OME4Q1B>
2020-10-23 21:37:21 +0900

    테스트진행중

--- hpen-new/src/test/com/hacademy/test/Test04_TransparentFrame.java
+++ hpen-new/src/test/com/hacademy/test/Test04_TransparentFrame.java
@@ -1,56 +1,66 @@
 package com.hacademy.test;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-import org.junit.Test;
-
 public class Test04_TransparentFrame {
-	@Test
-	public void test() throws Exception{
-		JFrame frame = new JFrame() {
-			int x = 0, y = 0;
-			boolean flag = false;
-			@Override
-			public void paint(Graphics g) {
-				System.out.println(getBackground());
-				g.setColor(getBackground());
-//				g.setColor(Color.black);
-				g.drawLine(x, 0, x, getWidth());
-				g.drawLine(0, y, getHeight(), y);
-				if(flag) {
-					x++; y++;
-					if(x > 500 || y > 500) flag = false;
+	static class CustomFrame extends JFrame{
+		int x = 0, y = 0;
+		boolean flag = false;
+		BufferedImage bg;
+		CustomFrame(){
+			setResizable(false);
+			setUndecorated(true);
+			setExtendedState(MAXIMIZED_BOTH);
+			setBackground(new Color(0,0,0,122));
+			setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			addMouseMotionListener(new MouseAdapter() {
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					x = e.getX();
+					y = e.getY();
 				}
-				else {
-					x--; y--;
-					if(x < 0 || y < 0) flag = true;
+			});
+			addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+						System.exit(0);
 				}
+			});
+			setVisible(true);
+		}
+		@Override
+		public void paint(Graphics g) {
+			if(bg == null) {
+				bg = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 			}
-		};
-		frame.setBounds(0,0,500,500);
-		frame.setResizable(false);
-		frame.setUndecorated(true);
-		frame.setBackground(new Color(0,0,0,122));
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-					System.exit(0);
-			}
-		});
-		frame.setVisible(true);
+			Graphics2D g2d = (Graphics2D)bg.getGraphics();
+			g2d.clearRect(0, 0, getWidth(), getHeight());
+			g2d.setComposite(AlphaComposite.Clear);
+			g2d.fillRect(0, 0, getWidth(), getHeight());
+			g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
+			g.setColor(Color.black);
+			g.drawLine(x, 0, x, getWidth());	
+			g.drawLine(0, y, getHeight(), y);
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		CustomFrame frame = new CustomFrame();
 		while(true) {
 			frame.repaint();
 			Thread.sleep(100);
 		}
-//		Thread.currentThread().join();
 	}
 }
 
