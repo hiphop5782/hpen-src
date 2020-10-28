1ac0cbc399ff7a9816ce4906743153203c442e7e
hiphop5782 <zdlsz@hanmail.net>
2020-10-28 15:21:45 +0900

    formlayout 테스트중

--- hpen-new/src/test/com/hacademy/test/Test09_FormLayoutTest.java
+++ hpen-new/src/test/com/hacademy/test/Test09_FormLayoutTest.java
@@ -1,10 +1,10 @@
 package com.hacademy.test;
 
-import java.awt.Color;
-
 import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
@@ -15,6 +15,31 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Test09_FormLayoutTest {
+	
+	public static JLabel label(String text) {
+		JLabel label = new JLabel(text, JLabel.CENTER);
+		label.setOpaque(false);
+		return label;
+	}
+	public static JCheckBox checkbox() {
+		JCheckBox checkbox = new JCheckBox();
+		return checkbox;
+	}
+	public static JRadioButton radio(String text) {
+		JRadioButton radio = new JRadioButton(text);
+		return radio;
+	}
+	public static JRadioButton radioCk(String text) {
+		JRadioButton radio = radio(text);
+		radio.setSelected(true);
+		return radio;
+	}
+	public static JTextField field(String text) {
+		JTextField field = new JTextField();
+		field.setText(text);
+		return field;
+	}
+	
 	static class TestFrame extends JFrame{
 		TestFrame(){
 			screen();
@@ -25,44 +50,30 @@ public class Test09_FormLayoutTest {
 		}
 		void screen() {
 			CellConstraints cc = new CellConstraints();
-			DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+			FormLayout layout = new FormLayout(
+				"100px,15px,100px,15px,100px,15px,100px"
+			);
+			DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+			
 			builder.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-			builder.appendColumn("right:pref");
-			builder.appendColumn("3dlu");;
-			builder.appendColumn("pref");
-			builder.appendColumn("5dlu");
-			builder.appendColumn("right:pref");
-			builder.appendColumn("3dlu");
-			builder.appendColumn("pref");
-			
-			builder.append("First:", new JTextField(), 3);
 
-	        builder.append("Last:", new JTextField());
+			builder.appendSeparator("캡쳐 옵션");
+			builder.append(label("화면 정지"), checkbox());
 	        builder.nextLine();
-
-	        builder.append("Married:", new JCheckBox());
+	        builder.append(label("저장 방식"));
+	        builder.append(radioCk("클립보드"), radio("임시파일"), radio("물어보기"));
 	        builder.nextLine();
-
-	        builder.append("Phone:", new JTextField());
+	        builder.appendSeparator("마우스 가이드 설정");
+	        builder.append(label("마우스 표시"), checkbox());
 	        builder.nextLine();
-
-	        builder.append("Fax:", new JTextField());
+	        builder.append(label("가이드 표시"), checkbox());
 	        builder.nextLine();
-
-	        builder.append("Email:", new JTextField());
+	        builder.append(label("가이드 두께"));
+	        builder.append(radio("가늘게"), radioCk("보통"), radio("두껍게"));
 	        builder.nextLine();
-
-	        builder.appendSeparator("Work");
-
-	        builder.append("Company:", new JTextField());
-	        builder.nextLine();
-
-	        builder.append("Phone:", new JTextField());
-	        builder.nextLine();
-
-	        builder.append("Fax:", new JTextField());
-	        builder.nextLine();
-			
+	        
+	        
+	        
 			setContentPane(builder.getContainer());
 		}
 	}
